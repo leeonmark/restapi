78ca76600d6e1a5ae1d30370d48a901647022d84
ra <lilyzahra9@gmail.com>
2021-11-19 15:36:13 +0700

    berhasil menampilkan semua data ethernet

--- controller.js
+++ controller.js
@@ -4,5 +4,16 @@ var response = require('./res');
 var connection = require('./koneksi');
 
 exports.index = function(req,res){
-    response.ok("Aplikasi REST API dabbest!!")
+    response.ok("REST API punyaku paling dabbest!!",res)
+};
+
+//menampilkan semua data ethernet
+exports.tampilsemuaethernet = function(req,res){
+    connection.query('SELECT * FROM ethernet', function(error, rows, fileds){
+        if(error){
+            connection.log(error);
+        }else {
+            response.ok(rows, res)
+        }
+    });
 };
