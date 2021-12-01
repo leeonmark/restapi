bf4919566ce24cdd3e3fb1ea37fc054c1ca79a63
ra <lilyzahra9@gmail.com>
2021-12-01 10:50:35 +0700

    try again

--- server.js
+++ server.js
@@ -2,6 +2,16 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 
+const port = 5000
+
+app.get("/",(req,res) => {
+    res.send("tes tes")
+});
+
+app.listen(process.env.PORT || port, () => {
+    console.log("you did it!")
+});
+
 //parser application/json
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
