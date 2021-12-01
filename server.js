c387f29ee1cb6541e09b248aa956d90e894be711
ra <lilyzahra9@gmail.com>
2021-12-01 10:05:21 +0700

    neo got my back

--- server.js
+++ server.js
@@ -1,20 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-const http = require('http');
-
-const hostname = '0.0.0.0';
-const port = 3000;
-
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('restapi');
-});
-
-server.listen(port, hostname, () => {
-    console.log('Server running at http://${hostname}:$(port)/');
-});
 
 //parser application/json
 app.use(bodyParser.urlencoded({extended: true}));
