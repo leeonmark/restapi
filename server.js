ec67dd82db93f8666bcf3a663d3cdb8d98d7f021
ra <lilyzahra9@gmail.com>
2021-12-01 09:52:15 +0700

    try to heroku

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+const http = require('http');
 
+const hostname = '0.0.0.0';
+const port = 3000;
 
-//parse application/json
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('restapi');
+});
+
+server.listen(port, hostname, () => {
+    console.log('Server running at http://${hostname}:$(port)/');
+});
+
+//parser application/json
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
