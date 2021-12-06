85dd86e29ca77fd9213e113dde5d0033682f89ee
ra <lilyzahra9@gmail.com>
2021-12-06 17:45:12 +0700

    fourth

--- server.js
+++ server.js
@@ -2,8 +2,6 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 
-app.use(bodyParser.urlencoded({extended: true}));
-
 app.get("/",(req,res) => {
     res.send("bismillah dulu")
 });
